4b4bd0cc8e8ffc3a77dbdae4e30c0120b6cdc497
kowska <jakarkow@gmail.com>
2016-12-03 10:33:06 -0500

    Can import multiple data srcs.

---
+++ bin/serialize.js
@@ -0,0 +1,108 @@
+var D3_PATH = '/vendor/assets/d3.min.js';
+
+var fs   = require('fs'),
+	sys  = require('system'),
+	page = require('webpage').create();
+
+if (sys.args.length < 1) { 
+	console.log("Missing arguments.");
+	phantom.exit(1);
+}
+else {
+	var gem_dir, id, scripts, styles, data, options, style, image_path;
+ 
+	try {
+	    gem_dir    =            sys.args[1];
+        scripts    = JSON.parse(sys.args[2]);
+     	styles     = JSON.parse(sys.args[3]);
+     	data       = JSON.parse(sys.args[4]);
+     	include_js = JSON.parse(sys.args[5]);
+     	options    = JSON.parse(sys.args[6]);
+     	id         = options['id']
+	} catch(err) {
+		console.log("Error parsing arguments.");
+		console.log(sys.args);
+		console.log(err)
+		page.close();
+		phantom.exit(1);
+	}
+
+	loadJSfile(gem_dir + D3_PATH);
+
+
+	var file_dest = options['file_dest'];
+	var file_name = options['file_name'];
+	var ext       = options['ext'];
+	// var quality   = options['quality'  ];
+
+    style  = "<style>"
+	for (i in styles) {
+    	style += fs.read(styles[i]);
+    }
+    style += "</style>"
+
+
+	for (i in include_js) {
+		console.log(include_js[i])
+	    loadJSfile(include_js[i]);
+	}
+
+    for (i in data) {
+    	loadJSfile(data[i]);
+    }
+
+ 	for (i in scripts) {
+    	loadJSfile(scripts[i]);
+    }
+
+    try{
+    	eval(window.onload());
+	}
+	catch(err){
+		console.log(err);
+		page.close();
+		phantom.exit(0);
+	}
+
+    var svg      = new XMLSerializer().serializeToString(d3.select(id).node());  
+	page.content = style + svg;
+
+	return page.content
+
+	page.close();
+	phantom.exit(0);
+	
+	
+};
+
+
+function getFileNameFromPath(file_path){
+	var file_name = file_path.split('\\').pop().split('/').pop();
+    return file_name.substring(0, file_name.length - 3);
+}
+
+function loadJSfile(file_path){
+	if (!phantom.injectJs(file_path)) {
+		console.log('Error loading: ' + file_path);
+		// phantom.exit(1);
+	}
+}	
+
+function readFile(file_path){
+	try {fs.read(file_path);}
+	catch(err) {
+		console.log('Unable to load ' + file_path);
+		console.log(err);
+		phantom.exit(1);
+	}
+
+}
+
+
+// output an img file with error msg??
+   
+// https://medium.com/@stockholmux/besting-phantomjs-font-problems-ee22795f5c0b#.5gyuus8p8
+// otherwise just use system fonts
+
+// linking external styles sheets through js : http://stackoverflow.com/questions/574944/how-to-load-up-css-files-using-javascript
+
--- lib/candid.rb
+++ lib/candid.rb
@@ -16,7 +16,7 @@ class Candid
         # options[:ext_assets]   ||= options[:ext_assets].to_s
         # options[:images]       ||= options[:images].to_s
 
-        include_js = include_js.map{ |data| include_raw_data(data) }
+        include_js = include_js.each_with_index.map  { |data, i| include_raw_data(data, i) }
 
 
         p include_js
@@ -36,17 +36,40 @@ class Candid
 		# return options[:file_destination]
 	end
 
+     def self.serialize_svg_to_string(scripts = [], styles = [], data_src = [], include_js = [], options = {})
+     
+        options[:id]         ||= 'svg'
+        options[:file_name]  ||= options[:file_name]
+        options[:ext]        ||= 'png'
+
+
+        include_js = include_js.each_with_index.map  { |data, i| include_raw_data(data, i) }
+
+
+        styles     = styles.to_s
+        scripts    = scripts.to_s
+        include_js = include_js.to_s
+        data_src   = data_src.to_s
+        options    = hash_to_s(options)
+        
+        
+        system 'js', candid_gem_root_path+'/bin/serialize.js', candid_gem_root_path, scripts, styles, data_src, include_js, options
+      
+        # TODO return file path of new image
+        # return options[:file_destination]
+    end
+
 
     def self.save_image(name, dest_path)
         # TODO 
     end
 
     private
-    def self.include_raw_data(raw_data)
+    def self.include_raw_data(raw_data, i)
 
         data         = raw_data[:data].to_json
         reference_as = raw_data[:reference_as]
-        file_name    = 'test.js'
+        file_name    = 'test' + i.to_s + '.js'
         
         # unless Dir.exist? tmp_dir
         #     make_tmp_dir
